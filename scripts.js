763b8c6b76aae42d9f2fa17ddb0c3dc46b89083d
octoadmin <info@octomarketing.onmicrosoft.com>
2016-08-30 19:11:01 -0600

    discoved we need to do event delegation

--- scripts.js
+++ scripts.js
@@ -7,18 +7,23 @@ var $readBookmarksButton = $('#readBookmarksButton');
 var $unreadBookmarksButton = $('#unreadBookmarksButton');
 var $clearReadBookmarksButton = $('#clearReadBookmarksButton');
 var $bookmarkCollection = $('#bookmarkCollection');
+var $bookmarkReadCheckbox = $('.bookmarkReadCheckbox')
 
 $addButton.on('click', function() {
   var titleInputValue = $titleInput.val();
   var urlInputValue = $urlInput.val();
-  // var bookmark = $('<figure class="bookmark">' + titleInputValue + '</figure>');
   var bookmark = $('<figure class="bookmark"><h3 class="bookmarkTitle">' + titleInputValue + '</h3><p class="bookmarkURL">' + urlInputValue + '</p><input type="checkbox" class="bookmarkReadCheckbox">Read</input><button type="button" class="removeBookmarkButton">Remove</button></figure>');
   $bookmarkCollection.prepend(bookmark);
+  $bookmarkReadCheckbox = $('.bookmarkReadCheckbox');
 })
 
-
-//we should have an input for bookmark title and an input field for bookmark url
-//we should have a function that takes both elements and adds them to the page
-//we should have a function that marks a bookmark as read - change css class?
-  //
-//
+// investigate event delegation
+$bookmarkReadCheckbox.on('click', function() {
+// $bookmarkReadCheckbox.change( function() {
+  console.log('ping')
+  // if(this.checked) {
+  //   this.parent().addClass('read')
+  // } else {
+  //   this.parent().removeClass('read')
+  // }
+});

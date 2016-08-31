4468cc2dce4c63e03d3811e3002b2c8fce5c214d
octoadmin <info@octomarketing.onmicrosoft.com>
2016-08-31 11:17:26 -0600

    implmented the error prompt if title or url is missing

--- scripts.js
+++ scripts.js
@@ -10,13 +10,21 @@ var $bookmarkCollection = $('#bookmarkCollection');
 var $bookmarkReadCheckbox = $('.bookmarkReadCheckbox');
 var $removeBookmarkButton = $('.removeBookmarkButton');
 
+function clearErrors() {
+  $errorPrompt.text('')
+};
+
 $addButton.on('click', function() {
   var titleInputValue = $titleInput.val();
   var urlInputValue = $urlInput.val();
   var bookmark = $('<figure class="bookmark"><h3 class="bookmarkTitle">' + titleInputValue + '</h3><p class="bookmarkURL">' + urlInputValue + '</p><input type="checkbox" class="bookmarkReadCheckbox">Read</input><button type="button" class="removeBookmarkButton">Remove</button></figure>');
+  if(titleInputValue === "" || urlInputValue === "") {
+    $errorPrompt.text('*Please fill in a bookmar title and url')
+  } else {
   $bookmarkCollection.prepend(bookmark);
   $bookmarkReadCheckbox = $('.bookmarkReadCheckbox');
-})
+  clearErrors();
+}})
 
 $('main').on('change', '.bookmarkReadCheckbox', function() {
   if ($(this).is(':checked')){
